450e6bdec8156f51c42196a3e97f5e6c6e2fd6ce
ddynikov <ddynikov@gmail.com>
2021-05-03 20:31:34 +0300

    db fixed

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	user: process.env.DB_USER,
+	host: process.env.DB_HOST,
+	database: process.env.DB_DATABASE,
+	password: process.env.DB_PASSWORD,
+	port: process.env.DB_PORT,
+	key: process.env.jwt_secret_key,
+	ssl: { rejectUnauthorized: false },
+}
